0fa1ad13a6fd2812c90a17f3c7079005d007bae7
Kunal Tyagi <kunaltyagi93@gmail.com>
2021-04-12 20:18:35 +0530

    added new javascript project

---
+++ Loan Calculator/app.js
@@ -0,0 +1,10 @@
+function calculateLoan(){
+    const amount = document.querySelector('#amount').value;
+    const interest_rate = document.querySelector('#interest').value;
+    const months = document.querySelector('#months').value;
+    const interest = (amount * (interest_rate * 0.01)) / months;
+    const payment = ((amount / months) + interest).toFixed(2);
+
+
+    document.querySelector('#payment').innerHTML =  'EMI: ₹' + payment;
+}

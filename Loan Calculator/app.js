11e54473a7306d6319764cfc201b2dc4d0f4a0a9
kunaltyagi9 <kunaltyagi93@gmail.com>
2023-05-10 12:22:10 +0530

    modified ui and backend logic of the application

--- Loan Calculator/app.js
+++ Loan Calculator/app.js
@@ -1,10 +1,25 @@
-function calculateLoan(){
-    const amount = document.querySelector('#amount').value;
-    const interest_rate = document.querySelector('#interest').value;
-    const months = document.querySelector('#months').value;
-    const interest = (amount * (interest_rate * 0.01)) / months;
-    const payment = ((amount / months) + interest).toFixed(2);
+window.onload = () => {
+    const inputs = document.querySelectorAll("input");
 
+    inputs.forEach(input => {
+        input.addEventListener('change', calculateLoan)
+    })
+}
 
-    document.querySelector('#payment').innerHTML =  'EMI: ₹' + payment;
+function calculateLoan () {
+    const principal = document.querySelector('#amount').value;
+    const interestRate = document.querySelector('#interest').value;
+    const tenure = document.querySelector('#tenure').value;
+
+    if (!principal || !interestRate || !tenure) return;
+
+    const monthlyInterest = interestRate / 100 / 12;
+    const emi = principal * monthlyInterest * Math.pow(1 + monthlyInterest, tenure) / (Math.pow(1 + monthlyInterest, tenure) - 1);
+
+    const totalAmount = emi * tenure;
+    const totalInterest = totalAmount - principal;
+
+    document.querySelector('#emi').innerText = 'EMI: ₹' + emi.toFixed(2);
+    document.querySelector('#totalAmount').innerText = 'Total Amount: ₹' + totalAmount.toFixed(2);
+    document.querySelector('#totalInterest').innerHTML = 'Total Interest: ₹' + totalInterest.toFixed(2);
 }

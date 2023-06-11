e3669325b97a88f3cbb43f98bc4ddff54b0b95ac
kunaltyagi9 <kunaltyagi93@gmail.com>
2023-06-11 12:56:35 +0530

    updated the code for countdown

--- countdown-timer/app.js
+++ countdown-timer/app.js
@@ -1,49 +1,50 @@
 window.onload = () => {
-    document.querySelector("#calculate").onclick = calculate;
-    document.querySelector("#reset").onclick = resetTime;
+    document.querySelector('#calculate').onclick = calculate;
+    document.querySelector('#reset').onclick = reset;
 }
 
-function calculate() {
+function calculate () {
     const date = document.querySelector("#date").value;
-    const time = document.querySelector("#time").value; 
+    const time = document.querySelector("#time").value;
 
-    const stop = document.querySelector("#stop");
+    const stop = document.querySelector('#stop');
     
     const endTime = new Date(date + " " + time);
 
-    const myInterval = setInterval(() => calculateTime(endTime), 1000);
+    const interval = setInterval(() => calculateTime(endTime), 1000);
 
     stop.addEventListener('click', () => {
-        clearInterval(myInterval);
+        clearInterval(interval);
     })
 }
 
-const calculateTime = (endTime) => {
+function calculateTime(endTime) {
     const currentTime = new Date();
 
     const days = document.querySelector('#countdown-days');
     const hours = document.querySelector('#countdown-hours');
     const minutes = document.querySelector('#countdown-minutes');
     const seconds = document.querySelector('#countdown-seconds');
-    
-    if(endTime > currentTime) {
+
+    if (endTime > currentTime) {
         const timeLeft = (endTime - currentTime) / 1000;
 
-        days.innerText = Math.floor(timeLeft / (3600 * 24));
-        hours.innerText = Math.floor((timeLeft / 3600) % 24);
+        console.log(timeLeft);
+        days.innerText = Math.floor(timeLeft / (24 * 60 * 60));
+        hours.innerText = Math.floor((timeLeft / (60 * 60)) % 24);
         minutes.innerText = Math.floor((timeLeft / 60) % 60);
         seconds.innerText = Math.floor(timeLeft % 60);
     } else {
-        days.innerText = 0;
-        hours.innerText = 0;
-        minutes.innerText = 0;
-        seconds.innerText = 0;
+        days.innerText = 0
+        hours.innerText = 0
+        minutes.innerText = 0
+        seconds.innerText = 0
     }
 }
 
-function resetTime() {
-    const days = document.querySelector('#countdown-days').innerText = 0;
-    const hours = document.querySelector('#countdown-hours').innerText = 0;
-    const minutes = document.querySelector('#countdown-minutes').innerText = 0;
-    const seconds = document.querySelector('#countdown-seconds').innerText = 0;
+function reset() {
+    document.querySelector('#countdown-days').innerText = 0;
+    document.querySelector('#countdown-hours').innerText = 0;
+    document.querySelector('#countdown-minutes').innerText = 0;
+    document.querySelector('#countdown-seconds').innerText = 0;
 }
